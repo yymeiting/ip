9ad488abf0d7af1ab45ba0a18ac150da3f6529c9
yymeiting <yymeiting@gmail.com>
2025-08-26 00:36:17 +0800

    Implement Level-6

--- src/main/java/CuteOwl.java
+++ src/main/java/CuteOwl.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CuteOwl {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100]; // Assume that there will be no more than 100 tasks
+        ArrayList<Task> tasks = new ArrayList<>();
         int t_count = 0; // Tasks counter
         String indentation = "    ";
+        String line = "    ____________________________________________________________\n";
 
-        String intro_text = indentation + "____________________________________________________________\n" +
-                indentation + " Hello! I'm CuteOwl :)\n" +
-                indentation + " What can I do for you?\n" +
-                indentation + "____________________________________________________________\n";
+        String intro_text = line + indentation + " Hello! I'm CuteOwl :)\n" +
+                indentation + " What can I do for you?\n" + line;
         System.out.println(intro_text);
 
         while (true) {
@@ -19,102 +19,129 @@ public class CuteOwl {
 
                 // Exit program if user input = "bye"
                 if (input.equalsIgnoreCase("bye")) {
-                    String exit_text = indentation + "____________________________________________________________\n" +
-                            indentation + " Bye. Hope to see you again soon!\n" +
-                            indentation + "____________________________________________________________\n";
+                    String exit_text = line + indentation + " Bye. Hope to see you again soon!\n" + line;
                     System.out.println(exit_text);
                     break;
                 }
 
                 // List task if user input = "list"
                 else if (input.equalsIgnoreCase("list")) {
-                    String list_tasks = indentation + "____________________________________________________________\n" +
-                            indentation + " Here are the tasks in your list:\n";
+                    String list_tasks = line + indentation + " Here are the tasks in your list:\n";
                     for (int  i = 0; i < t_count; i+=1 ) {
-                        list_tasks = list_tasks + indentation + " " + (i + 1) + ". " + tasks[i] + "\n";
+                        list_tasks = list_tasks + indentation + " " + (i + 1) + ". " + tasks.get(i) + "\n";
                     }
 
-                    list_tasks = list_tasks + indentation + "____________________________________________________________\n";
+                    list_tasks = list_tasks + line;
                     System.out.println(list_tasks);
                 }
 
                 // Mark task with "X" if user input = "mark number"
-                else if (input.startsWith("mark ")) {
+                else if (input.startsWith("mark")) {
                     int list_index = Integer.parseInt(input.substring(5)) - 1;
                     if (list_index >= 0 && list_index < t_count) {
-                        tasks[list_index].mark();
-                        String mark_text = indentation + "____________________________________________________________\n" +
-                                indentation + " Nice! I've marked this task as done:\n" +
-                                indentation + " " + tasks[list_index] + "\n" +
-                                indentation + "____________________________________________________________\n";
+                        tasks.get(list_index).mark();
+                        String mark_text = line + indentation + " Nice! I've marked this task as done:\n" +
+                                indentation + " " + tasks.get(list_index) + "\n" + line;
                         System.out.println(mark_text);
                     }
                 }
 
                 // Unmark task if user input = "unmark number"
-                else if (input.startsWith("unmark ")) {
+                else if (input.startsWith("unmark")) {
                     int list_index = Integer.parseInt(input.substring(7)) - 1;
                     if (list_index >= 0 && list_index < t_count) {
-                        tasks[list_index].unmark();
-                        String unmark_text = indentation + "____________________________________________________________\n" +
-                                indentation + " OK, I've marked this task as not done yet:\n" +
-                                indentation + " " + tasks[list_index] + "\n" +
-                                indentation + "____________________________________________________________\n";
+                        tasks.get(list_index).unmark();
+                        String unmark_text = line + indentation + " OK, I've marked this task as not done yet:\n" +
+                                indentation + " " + tasks.get(list_index) + "\n" + line;
                         System.out.println(unmark_text);
                     }
                 }
 
                 // todo task if user input = "todo"
-                else if (input.startsWith("todo ")) {
+                else if (input.startsWith("todo")) {
                     String description = input.length() > 5 ? input.substring(5).trim() : "";
 
                     if (description.isEmpty()) {
                         throw new CuteOwlException("OOPS!!! The description of a todo cannot be empty.");
                     }
-                    tasks[t_count] = new Todo(description);
+                    tasks.add(new Todo(description));
                     t_count += 1;
                 }
 
                 // deadline task if user input = "deadline"
-                else if (input.startsWith("deadline ")) {
+                else if (input.startsWith("deadline")) {
+                    String description = input.length() > 9 ? input.substring(9).trim() : "";
+
+                    if (description.isEmpty()) {
+                        throw new CuteOwlException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+
+                    if (!description.contains("/by")) {
+                        throw new CuteOwlException("OOPS!!! Please enter your deadline in the correct format\n" +
+                                indentation + " (i.e. deadline <task> /by <date>). ");
+                    }
+
                     String[] parts = input.substring(9).split("/by");
-                    String description = parts[0].trim();
+                    String event_desc = parts[0].trim();
                     String by = parts[1].trim();
-                    tasks[t_count] = new Deadline(description, by);
+                    tasks.add(new Deadline(event_desc, by));
                     t_count += 1;
                 }
 
-                // deadline task if user input = "event"
-                else if (input.startsWith("event ")) {
+                // event task if user input = "event"
+                else if (input.startsWith("event")) {
+                    String description = input.length() > 6 ? input.substring(6).trim() : "";
+
+                    if (description.isEmpty()) {
+                        throw new CuteOwlException("OOPS!!! The description of a event cannot be empty.");
+                    }
+
+                    if (!description.contains("/from") &&  !description.contains("/to")) {
+                        throw new CuteOwlException("OOPS!!! Please enter your event in the correct format\n" + indentation +
+                                " (i.e. event <task> /from <date> /to <date>). ");
+                    }
+
                     String[] parts = input.substring(6).split("/from|/to");
-                    String description = parts[0].trim();
+                    String event_desc = parts[0].trim();
                     String from = parts[1].trim();
                     String to = parts[2].trim();
-                    tasks[t_count] = new Event(description, from, to);
+                    tasks.add(new Event(event_desc, from, to));
                     t_count += 1;
-                } else {
+                }
+
+                // delete task if user input = "delete"
+                else if (input.startsWith("delete")) {
+                    int index = Integer.parseInt(input.substring(7).trim()) - 1;
+
+                    if (index < 0 || index >= t_count) {
+                        throw new CuteOwlException("OOPS!!! The index out of bounds.");
+                    }
+
+                    Task removedTask = tasks.remove(index);
+                    t_count -= 1;
+                    String delete_text = line + indentation + " Noted. I've removed this task:\n" +
+                            indentation + "   " + removedTask + "\n" +
+                            indentation + " Now you have " + tasks.size() + " tasks in the list.\n" + line;
+                        System.out.println(delete_text);
+                }
+
+                else {
                     throw new CuteOwlException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
 
                 // Print task after user input
-                if (input.startsWith("event ") || input.startsWith("deadline ") || input.startsWith("todo ")) {
-                    String task_text =  indentation + "____________________________________________________________\n" +
-                            indentation + " Got it. I've added this task:\n" +
-                            indentation + "   " + tasks[t_count - 1] + "\n" +
-                            indentation + " Now you have " + t_count + " tasks in the list.\n" +
-                            indentation + "____________________________________________________________\n";
+                if (input.startsWith("event") || input.startsWith("deadline") || input.startsWith("todo")) {
+                    String task_text =  line + indentation + " Got it. I've added this task:\n" +
+                            indentation + "   " + tasks.get(t_count - 1) + "\n" +
+                            indentation + " Now you have " + t_count + " tasks in the list.\n" + line;
                     System.out.println(task_text);
                 }
 
             } catch (CuteOwlException e) {
-                String error_text = indentation + "____________________________________________________________\n" +
-                        indentation + " " + e.getMessage() + "\n" +
-                        indentation + "____________________________________________________________\n";
+                String error_text = line + indentation + " " + e.getMessage() + "\n" + line;
                 System.out.println(error_text);
             } catch (Exception e) {
-                String error_text = indentation + "____________________________________________________________\n" +
-                        indentation + " " + e.getMessage() + "\n" +
-                        indentation + "____________________________________________________________\n";
+                String error_text = line + indentation + " " + e.getMessage() + "\n" + line;
                 System.out.println(error_text);
             }
 
