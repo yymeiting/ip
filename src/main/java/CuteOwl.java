4806e697d44c7371630e16ade426b1d98a4c821b
yymeiting <yymeiting@gmail.com>
2025-08-25 17:47:28 +0800

    Implement Level-0

---
+++ src/main/java/CuteOwl.java
@@ -0,0 +1,11 @@
+public class CuteOwl {
+    public static void main(String[] args) {
+        String intro_text = "____________________________________________________________\n" +
+                " Hello! I'm CuteOwl :) \n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(intro_text);
+    }
+}
