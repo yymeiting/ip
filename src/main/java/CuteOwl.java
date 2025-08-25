954e1b5832089681cdd3cbf261793e475e3bdd05
yymeiting <yymeiting@gmail.com>
2025-08-25 17:49:38 +0800

    Implement Level-1

--- src/main/java/CuteOwl.java
+++ src/main/java/CuteOwl.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class CuteOwl {
     public static void main(String[] args) {
-        String intro_text = "____________________________________________________________\n" +
-                " Hello! I'm CuteOwl :) \n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String indentation = "    ";
+        String intro_text = indentation + "____________________________________________________________\n" +
+                indentation + " Hello! I'm CuteOwl :) \n" +
+                indentation + " What can I do for you?\n" +
+                indentation + "____________________________________________________________\n";
         System.out.println(intro_text);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                String exit_text = indentation + "____________________________________________________________\n" +
+                        indentation + " Bye. Hope to see you again soon! \n" +
+                        indentation + "____________________________________________________________\n";
+                System.out.println(exit_text);
+                break;
+            }
+            String echo_text = "\n" + indentation + "____________________________________________________________\n" +
+                    indentation + " " + input + "\n" +
+                    indentation + "____________________________________________________________\n";
+            System.out.println(echo_text);
+        }
+
+        scanner.close();
     }
 }
