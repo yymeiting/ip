833ab1966ecd7765dfa8b0b52defdfd16f6c494e
yymeiting <yymeiting@gmail.com>
2025-08-29 15:37:32 +0800

    Add Storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class Storage {
+    Path filePath = Paths.get("data", "cuteOwl.txt");
+
+    public ArrayList<Task> load() {
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        try {
+            // if file does not exist, create file
+            if (!Files.exists(filePath)) {
+                Files.createDirectories(filePath.getParent());
+                Files.createFile(filePath);
+                return tasks;
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading tasks: " + e.getMessage());
+        }
+        return tasks;
+    }
+
+    public void save(ArrayList<Task> tasks) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()));
+            for (Task task : tasks) {
+                writer.write(task.toSaveFormat());
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error saving tasks: " + e.getMessage());
+        }
+    }
+}
