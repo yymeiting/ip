2f4c1e5d69e3cc45d8ff54197c0e399c8dca6363
yymeiting <yymeiting@gmail.com>
2025-08-29 15:41:26 +0800

    Add toSaveFormat into Deadline, Event, Task and Todo classes

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -11,4 +11,12 @@ public class Deadline extends Task {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 
+    @Override
+    public String toSaveFormat() {
+        return "D | " + (isDone ? "1" : "0") + " | " + description +
+                " | " + by ;
+    }
+
+
+
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,12 @@ public class Event extends Task {
     public String toString () {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to +")";
     }
+
+    @Override
+    public String toSaveFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description +
+                " | " + from + "-" + to;
+    }
+
+
 }
